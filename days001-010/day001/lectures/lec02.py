e2770abf2e2d553cac02ae38a83dfab897942259
shilobdas <drogonjang@gmail.com>
2026-01-11 01:27:58 +0600

    Project added

---
+++ days001-010/day001/lectures/lec02.py
@@ -0,0 +1,18 @@
+print("Hello world!")
+print("Hello world!")
+print("Hello world!")
+print()
+
+print("Hello world!\nHello world!")
+print()
+
+print("Hello world!\nHello world!\nHello world!")
+print()
+
+print("Hello" + "Shilob")
+print("Hello" + " Shilob")
+print("Hello " + "Shilob")
+print("Hello" + " " + "Shilob")
+
+# IndentationError
+#     print("Hello" + " " + "Angela")
